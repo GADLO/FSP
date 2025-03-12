eaaa326d4c1eb577a5c49de9999a1a0f0dd5be03
GADLO <geekarea@outlook.com>
2025-03-12 15:48:45 +0800

    update index.html

--- index-ws.js
+++ index-ws.js
@@ -4,7 +4,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', function (req, res) {
-    res.sendFile('index.html', { root: __dirname });
+    res.sendFile('wsExample.html', { root: __dirname });
 });
 
 server.on('request', app);
