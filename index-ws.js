26c6b86417f6870e0a73abe085252ed9300c7af8
GADLO <geekarea@outlook.com>
2025-03-13 10:54:33 +0800

    add database logging

--- index-ws.js
+++ index-ws.js
@@ -1,3 +1,4 @@
+const { log } = require('console');
 const express = require('express');
 const server = require('http').createServer();
 const app = express();
@@ -7,9 +8,22 @@ app.get('/', function (req, res) {
     res.sendFile('wsExample.html', { root: __dirname });
 });
 
+
 server.on('request', app);
 server.listen(port, () => console.log('server started on port 3000')
+
+
 )
+process.on('SIGINT', () => {
+    console.log('sigint');
+    wss.clients.forEach(function each(client) {
+        client.close();
+    });
+
+    server.close(() => {
+        shutdownDB()
+    })
+})
 
 
 // Begin websocket
@@ -27,10 +41,11 @@ wss.on('connection', function connection(ws) {
         ws.send('Welcome to my server');
     }
 
+    db.run(`INSERT INTO visitors (count, time) VALUES (${numClients}, datetime('now'))`)
+
     ws.on('close', function close() {
         wss.broadcast(`Current visitors: ${numClients}`);
         console.log('A client has disconnected');
-
     })
 
 })
@@ -38,4 +53,33 @@ wss.broadcast = function broadcast(data) {
     wss.clients.forEach(function each(client) {
         client.send(data)
     })
+}
+/** end websocket */
+
+/** begin database */
+const sqlite = require('sqlite3');
+const db = new sqlite.Database(':memory:');
+
+db.serialize(() => {
+    db.run(`
+        CREATE TABLE visitors (
+        count INTEGER,
+        time TEXT
+        )
+        `)
+})
+
+
+function getCounts() {
+    db.each("SELECT * FROM visitors", (err, row) => {
+        console.log(row);
+    })
+}
+
+
+function shutdownDB() {
+    getCounts();
+    console.log('Shutting down db');
+    db.close()
+
 }
